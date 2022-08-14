7b0a3e60751fd5265bfceace3801ccf5ca97939d
mirshod948 <74175984+mirshod948@users.noreply.github.com>
2022-08-14 09:23:38 +0500

    Update EKUB.php

--- misollar/OOP/Abstruction/EKUB.php
+++ misollar/OOP/Abstruction/EKUB.php
@@ -1,12 +1,35 @@
 <?php
 
 
-class Abs extends EKUB
-{
+namespace OOP;
 
-    public function getAll(){
-        $new = new EKUB();
-        $new->calcResult();
+
+class EKUB
+{
+    private $a;
+    private $b;
+
+    public function __construct($a, $b)
+    {
+        $this->a=$a;
+        $this->b=$b;
+    }
+    public function CalcEkub(){
+        $a=12;
+        $b=6;
+        while ($a != $b)
+        {
+            if ($a > $b) $a %= $b;
+            else $b %= $a;
+
+            if ($a == 0) $a = $b;
+            if ($b == 0) $b = $a;
+        }
+        return $a;
     }
 
+
 }
+
+$n = new EKUB(12, 6);
+echo "Resulat: ".$n->CalcEkub();
